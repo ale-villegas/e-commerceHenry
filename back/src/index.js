c66c468b2584f6a21a3c8d47d6ef63d373bcfc2f
Desialvi <desialvi@gmail.com>
2023-11-23 00:17:43 -0300

    Sincronizo Modelos con BBDD

--- back/src/index.js
+++ back/src/index.js
@@ -1,19 +1,13 @@
 
 const server = require('./server');
-//const {conn} = require('./DB_connection')    
+const {conn} = require('./DB_connection')    
 
 
-// conn.sync({force: true}).then(() => {
-// const PORT = 3001;
-// server. listen(PORT, () => {
-//    console.log(`El servidor está corriendo en el puerto ${PORT}`)
-// });
-
-// }
-// )
-
-
+conn.sync({force: false}).then(() => {
 const PORT = 3001;
-server.listen(PORT, () => {
+server. listen(PORT, () => {
     console.log(`El servidor está corriendo en el puerto ${PORT}`)
  });
+
+ }
+ )

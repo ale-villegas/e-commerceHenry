b63177e8c02c73d2e2ce39d948756a92e012d0a9
lt@genII <lautaro.trabado@hotmail.com>
2023-12-06 16:41:03 -0300

    Popover implementado

--- front/src/components/ShoppingCart.jsx
+++ front/src/components/ShoppingCart.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import IconButton from '@mui/material/IconButton';
 import Badge from '@mui/material/Badge';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCartOutlined';
 
+//Popover
+import Popover from '@mui/material/Popover';
+import Typography from '@mui/material/Typography';
+
 const ShoppingCart = ({ itemCount }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
   return (
-    <IconButton aria-label="Carrito de compras" color="inherit">
-      <Badge badgeContent={itemCount} color="error">
-        <ShoppingCartIcon />
-      </Badge>
-    </IconButton>
+    <div>
+      <IconButton aria-label="Carrito de compras" color="inherit" onClick={handleClick}>
+        <Badge badgeContent={itemCount} color="error">
+          <ShoppingCartIcon/>
+        </Badge>
+      </IconButton>
+      <Popover id={id} open={open} anchorEl={anchorEl} onClose={handleClose}
+              anchorOrigin={{vertical: 'bottom', horizontal: 'right',}}
+              transformOrigin={{vertical: 'top', horizontal: 'right',}}>
+        <Typography sx={{ p: 2 }}>Contenido del carrito aquí</Typography>
+      </Popover>
+    </div>
   );
 };
 

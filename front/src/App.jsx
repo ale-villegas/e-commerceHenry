033e44b1f637ee9d207eaf35aca5ab6a74a6b09c
ale-villegas <106825168+ale-villegas@users.noreply.github.com>
2023-12-20 14:52:30 +0100

    arreglando bug

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import ProductList from "./components/ProductList";
 import { Container, CssBaseline, ThemeProvider } from "@mui/material";
 
